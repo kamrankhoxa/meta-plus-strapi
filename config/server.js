f1e820033ecf26acb39909d0c08c728ccb0e1199
kamrankhoxa <k2455552@gmail.com>
2023-06-22 11:35:45 -0700

    update package.json

--- config/server.js
+++ config/server.js
@@ -7,3 +7,19 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+
+// module.exports = {
+//   apps: [
+//     {
+//       name: 'your-app-name', // Your project name
+//       cwd: '/home/ubuntu/my-project', // Path to your project
+//       script: 'npm', // For this example we're using npm, could also be yarn
+//       args: 'start', // Script to start the Strapi server, `start` by default
+//       env: {   
+//         ADMIN_JWT_SECRET: '6455aad034a64f765034a6f4727aeeb8',
+//         NODE_ENV: 'production',
+//       },
+//     },
+//   ],
+// };
