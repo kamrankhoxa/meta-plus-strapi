13965f5cf1d3a2ee5f05f4813d8bcb5f43f3dd7c
kamrankhoxa <k2455552@gmail.com>
2023-06-23 17:19:49 +0500

    updated Heroku config

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('MY_HEROKU_URL'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '6455aad034a64f765034a6f4727aeeb8'),
