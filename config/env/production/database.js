fe31ed43c9e95d847a9bf4d526a771f4fd492055
kamrankhoxa <k2455552@gmail.com>
2023-06-23 19:31:00 +0500

    make it better

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,16 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: env("DATABASE_CLIENT", "postgres"),
-        host: env("DATABASE_HOST", "ec2-52-31-161-46.eu-west-1.compute.amazonaws.com"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "dt896gb8qr3je"),
-        user: env("DATABASE_USERNAME", "wgruhkwmnwlbmb"),
-        password: env("DATABASE_PASSWORD", "314b277d680b9284bfef8d0f4bac4c048264378322a442e8a9480926f7c6a22a"),
-        ssl: false,
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
-      debug: false,
-      options: {},
+      options: {
+        useNullAsDefault: true,
+      },
     },
   },
 });
